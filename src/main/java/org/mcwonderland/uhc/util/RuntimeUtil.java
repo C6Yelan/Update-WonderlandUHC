7f914700ed47f0e04497cc9de715aa53f7307eed
C6Yelan <a0919458922@gmail.com>
2026-05-09 21:07:11 +0800

    fix(scoreboard): suppress unavailable TPS metric noise

--- src/main/java/org/mcwonderland/uhc/util/RuntimeUtil.java
+++ src/main/java/org/mcwonderland/uhc/util/RuntimeUtil.java
@@ -1,13 +1,16 @@
 package org.mcwonderland.uhc.util;
 
+import org.bukkit.Bukkit;
 import org.mcwonderland.uhc.legacy.LegacyFoundationAdapter;
 
 import java.lang.reflect.Field;
+import java.lang.reflect.Method;
 import java.text.DecimalFormat;
 
 public class RuntimeUtil {
 
     private static final DecimalFormat format = new DecimalFormat("00.00");
+    private static final double DEFAULT_TPS = 20D;
     private static Class<?> clazz = null;
     private static Object si = null;
     private static Field tpsField = null;
@@ -20,19 +23,24 @@ public class RuntimeUtil {
             si = clazz.getMethod("getServer").invoke(null);
             tpsField = si.getClass().getField("recentTps");
         } catch (Exception e) {
-            e.printStackTrace();
+            tpsField = null;
         }
     }
 
     public static double getTPS(int time) {
-        double[] tps = null;
+        double paperTps = getPaperTPS(time);
+        if (paperTps > 0D)
+            return paperTps;
+
+        if (tpsField == null || si == null)
+            return DEFAULT_TPS;
+
         try {
-            tps = ((double[]) tpsField.get(si));
+            double[] tps = (double[]) tpsField.get(si);
+            return getTPSAt(tps, time);
         } catch (Exception e) {
-            e.printStackTrace();
+            return DEFAULT_TPS;
         }
-        assert tps != null;
-        return tps[time];
     }
 
     public static DecimalFormat getTPSFormat() {
@@ -51,5 +59,22 @@ public class RuntimeUtil {
         return AvailableMemory() < fillMemoryTolerance;
     }
 
+    private static double getPaperTPS(int time) {
+        try {
+            Method getTps = Bukkit.getServer().getClass().getMethod("getTPS");
+            double[] tps = (double[]) getTps.invoke(Bukkit.getServer());
+            return getTPSAt(tps, time);
+        } catch (Exception ex) {
+            return -1D;
+        }
+    }
+
+    private static double getTPSAt(double[] tps, int time) {
+        if (tps == null || tps.length == 0)
+            return DEFAULT_TPS;
+
+        int index = Math.max(0, Math.min(time, tps.length - 1));
+        return tps[index];
+    }
 
 }
