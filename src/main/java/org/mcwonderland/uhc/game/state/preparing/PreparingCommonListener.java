2a1a7184257133bcd39a4730c16cd16bf90f0aad
C6Yelan <a0919458922@gmail.com>
2026-05-23 22:28:29 +0800

    fix(lobby): handle damage and respawn flow

--- src/main/java/org/mcwonderland/uhc/game/state/preparing/PreparingCommonListener.java
+++ src/main/java/org/mcwonderland/uhc/game/state/preparing/PreparingCommonListener.java
@@ -2,13 +2,22 @@ package org.mcwonderland.uhc.game.state.preparing;
 
 import org.mcwonderland.uhc.WonderlandUHC;
 import org.mcwonderland.uhc.game.state.share.CommonListener;
+import org.mcwonderland.uhc.platform.scheduler.PluginScheduler;
 import org.mcwonderland.uhc.practice.Practice;
+import org.mcwonderland.uhc.util.GameUtils;
+import org.mcwonderland.uhc.util.Lobby;
+import org.mcwonderland.uhc.util.PlayerUtils;
+import org.mcwonderland.uhc.util.UHCWorldUtils;
+import org.bukkit.World;
 import org.bukkit.entity.Entity;
 import org.bukkit.entity.Player;
+import org.bukkit.entity.Projectile;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.block.BlockBreakEvent;
+import org.bukkit.event.entity.CreatureSpawnEvent;
 import org.bukkit.event.entity.EntityDamageByEntityEvent;
 import org.bukkit.event.entity.EntityDamageEvent;
+import org.bukkit.event.entity.PlayerDeathEvent;
 
 public class PreparingCommonListener extends CommonListener {
 
@@ -22,7 +31,7 @@ public class PreparingCommonListener extends CommonListener {
             Player player = ( Player ) e.getEntity();
 
             if (!practice.isInPractice(player)) {
-                super.onDamage(e);
+                cancelLobbyPlayerDamage(e);
                 return;
             }
 
@@ -42,8 +51,75 @@ public class PreparingCommonListener extends CommonListener {
                 }
             }
         } else
-
             super.onDamage(e);
+    }
+
+    private void cancelLobbyPlayerDamage(EntityDamageEvent e) {
+        if (isPlayerCausedDamage(e))
+            e.setCancelled(true);
+    }
+
+    private boolean isPlayerCausedDamage(EntityDamageEvent e) {
+        return getPlayerDamager(e) != null;
+    }
+
+    private Player getPlayerDamager(EntityDamageEvent e) {
+        if (!(e instanceof EntityDamageByEntityEvent))
+            return null;
+
+        Entity damager = (( EntityDamageByEntityEvent ) e).getDamager();
+
+        if (damager instanceof Player)
+            return ( Player ) damager;
+
+        if (damager instanceof Projectile)
+            return PlayerUtils.getShooter(( Projectile ) damager);
+
+        return null;
+    }
+
+    @EventHandler
+    public void onDeath(PlayerDeathEvent e) {
+        Player player = e.getEntity();
+
+        if (practice.isInPractice(player))
+            return;
+
+        e.getDrops().clear();
+        e.setDroppedExp(0);
+        e.deathMessage(null);
+
+        respawnInLobby(player);
+    }
+
+    private void respawnInLobby(Player player) {
+        PluginScheduler.runLater(1, () -> {
+            if (!shouldHandleLobbyPlayer(player))
+                return;
+
+            PlayerUtils.respawnIfDead(player);
+
+            PluginScheduler.runLater(1, () -> {
+                if (shouldHandleLobbyPlayer(player))
+                    Lobby.send(player);
+            });
+        });
+    }
+
+    private boolean shouldHandleLobbyPlayer(Player player) {
+        return player.isOnline() && !practice.isInPractice(player) && GameUtils.isWaiting();
+    }
+
+    @EventHandler
+    public void onCreatureSpawn(CreatureSpawnEvent e) {
+        if (e.getSpawnReason() == CreatureSpawnEvent.SpawnReason.NATURAL && isSeparateLobbyWorld(e.getLocation().getWorld()))
+            e.setCancelled(true);
+    }
+
+    private boolean isSeparateLobbyWorld(World world) {
+        World lobby = UHCWorldUtils.getLobby();
+
+        return world != null && world.equals(lobby) && !UHCWorldUtils.isUhcWorld(world);
     }
 
 
